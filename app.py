44afc9d5cff1555d9392890597b713e93c8c669b
RodolfoTorija <112714490+RodolfoTorija@users.noreply.github.com>
2023-02-09 16:56:43 -0600

    cambio de archivos
    cambio

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/style.css")
+def style():
+    return render_template("style.css")
+
 if __name__ == "__main__":
     app.run(debug=True)
