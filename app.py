6b06c3da5000eda91d11ddce3a1eb7fb5e725b2c
RodolfoTorija <112714490+RodolfoTorija@users.noreply.github.com>
2023-02-09 17:49:04 -0600

    especialidades
    especialidades html

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/index.html')
+def index_inicio():
+    return render_template("index.html")
 
-@app.route("/style.css")
-def style():
-    return render_template("style.css")
+@app.route('/especialidades.html')
+def especialidades():
+    return render_template("especialidades.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
