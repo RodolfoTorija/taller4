05c7743e1be0473dc1098f96dfe0950bd6598545
RodolfoTorija <112714490+RodolfoTorija@users.noreply.github.com>
2023-02-21 17:21:55 -0600

    basede datos

--- app.py
+++ app.py
@@ -1,69 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 from flask_mysqldb import MySQL
-
+from jinja2 import Template
 
 app = Flask(__name__)
 
 
 
-# Configuración de la base de datos
-...
 
-
-try:
-    mydb = mysql.connector.connect(
-        host = "localhost",
-        port = "3306",
-        user= "root",
-        password = "Zeromainj0.",
-        database = "clinica_azul"
-    )
-
-    print("Conexión exitosa a la base de datos. \n")
-
-except mysql.connector.Error as error:
-    print("Error al conectarse a la base de datos: {}".format(error))
-    mydb = None
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-#Conexion de base de datos
-@app.route('/verificar', methods = ['POST'])
-def verificar():
-    if mydb is None:
-        return render_template('index.html', mensaje='Error al conectarse a la base de datos')
-
-    nombre = request.form['nombreusuario']
-    contrasena = request.form['contrasena']
-
-    try:
-        mycursor = mydb.cursor()
-
-        mycursor.execute("SELECT * FROM usuarios WHERE nombre = %s AND contrasena = %s", (nombre, contrasena))
-
-        usuario = mycursor.fetchone()
-
-        if usuario:
-            # Inicio de sesión exitoso
-            return redirect(url_for('index.html'))
-        else:
-            # Nombre de usuario o contraseña incorrectos
-            return render_template('index.html', mensaje='Nombre de usuario o contraseña incorrectos')
-
-    except mysql.connector.Error as error:
-        print("Error al ejecutar la consulta a la base de datos: {}".format(error))
-        return render_template('index.html', mensaje='Error al ejecutar la consulta a la base de datos')
-
-
-
-
-
-
 
 @app.route('/index.html')
 def index_inicio():
@@ -85,6 +34,56 @@ def quienessomos():
 @app.route('/contactanos.html')
 def contactanos():
     return render_template("contactanos.html")
+
+# Configuración de la base de datos
+...
+
+
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        port = "3306",
+        user= "root",
+        password = "Zeromainj0.",
+        database = "clinica_azul"
+    )
+
+    print("Conexión exitosa a la base de datos. \n")
+
+except mysql.connector.Error as error:
+    print("Error al conectarse a la base de datos: {}".format(error))
+    mydb = None
+
+#Conexion de base de datos
+@app.route('/verificar', methods = ['GET','POST'])
+def verificar():
+    if mydb is None:
+        return render_template('/index.html', mensaje='Error al conectarse a la base de datos')
+
+    nombre = request.form['form2Example11']
+    contrasena = request.form['form2Example22']
+
+    try:
+        mycursor = mydb.cursor()
+
+        mycursor.execute("SELECT * FROM usuarios WHERE nombre = %s AND contrasena = %s", (nombre, contrasena))
+
+        usuario = mycursor.fetchone()
+
+        if usuario:
+            print("Inicio correctametne")
+            # Inicio de sesión exitoso
+            return render_template('/index.html')
+        else:
+            print("No se inicio correctamente")
+            # Nombre de usuario o contraseña incorrectos
+            return render_template('/index.html', error='Nombre de usuario o contraseña incorrectos')
+
+    except mysql.connector.Error as error:
+        print("Error al ejecutar la consulta a la base de datos: {}".format(error))
+        return render_template('/index.html', error='Error al ejecutar la consulta a la base de datos')
+
+
 
 
 if __name__ == '__main__':
